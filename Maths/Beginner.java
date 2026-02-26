0ffb4fe92fc6c4ddd100d1ad696a390b6f5d985d
Vinay-Sandeep <133085003+VINAY-SANDEEP@users.noreply.github.com>
2026-02-26 13:29:03 +0000

    new

---
+++ Maths/Beginner.java
@@ -0,0 +1,22 @@
+public class Beginner {
+    public static void main(String[] args) {
+      System.out.println("Count Digits : "+count(123));
+      System.out.println("Reverse : "+reverse(123));
+      System.out.println("Palindrome : "+palin(121));
+    }
+    public static int count(int n){
+        //code 
+        
+        
+    }
+    public static int reverse(int n){
+        //code 
+        
+        
+    }
+    public static boolean palin(int n){
+        //code 
+        
+        
+    }
+}
